bd8e3e03fd8c529047d3640bbe963db1d8963d2c
LeeJunHee <leejunhee1219@snu.ac.kr>
2018-11-30 16:30:12 +0900

    2012-13401

---
+++ week13.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<iostream>
+
+using namespace std;
+
+void main() {
+	/*
+	TODO: replace "XXXX-XXXXX" to your student number
+	*/
+	cout << "My student number is 2012-13401" << endl;
+}
